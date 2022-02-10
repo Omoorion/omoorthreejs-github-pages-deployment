6715c38e9d4792f2eaf9b2270baefbc4c2da4b0b
Omoorion <86531601+Omoorion@users.noreply.github.com>
2022-02-10 19:05:28 +0200

    Updates

--- theme.js
+++ theme.js
@@ -3,6 +3,8 @@ const themeMap = {
     light: "solar",
     solar: "dark"
   };
+
+  //const dark = document.querySelector("section.bubble")
   
   const theme = localStorage.getItem('theme')
     || (tmp = Object.keys(themeMap)[0],
@@ -12,11 +14,17 @@ const themeMap = {
   bodyClass.add(theme);
   
   function toggleTheme() {
+
     const current = localStorage.getItem('theme');
+    console.log(themeMap[current])
     const next = themeMap[current];
-  
+
     bodyClass.replace(current, next);
     localStorage.setItem('theme', next);
+    //if(themeMap[current] == "dark"){
+      //console.log("balls")
+      //dark.style.background= "#ffffff";
+    //}
   }
   
   document.getElementById('themeButton').onclick = toggleTheme;
